9a92674d99522070ed2f43f46fab7a446c772f75
Abraham <cedenoabraham@gmail.com>
2020-07-24 01:23:22 -0300

    base config strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f64b3d1e3ada275e23266433cb898a50'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '78b9d62b-1a2a-44cc-9d16-08e27f89df9d'
+};
