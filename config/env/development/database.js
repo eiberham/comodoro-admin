5152bfc0b6e1b653b298ba53397c2d4ec5783b06
Abraham <cedenoabraham@gmail.com>
2020-07-28 22:56:43 -0300

    wipe data

--- config/env/development/database.js
+++ config/env/development/database.js
@@ -1,4 +1,4 @@
-module.exports = ({ env }) => ({
+/* module.exports = ({ env }) => ({
     defaultConnection: 'default',
     connections: {
       default: {
@@ -12,4 +12,24 @@ module.exports = ({ env }) => ({
         },
       },
     },
-  });
+  }); */
+
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', 'postgres'),
+        password: env('DATABASE_PASSWORD', 'postgres'),
+      },
+      options: {
+        ssl: false
+      },
+    },
+  },
+});
